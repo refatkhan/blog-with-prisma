78bc4b5df8c9c5fdb4caa7877d4708ffab75a393
Ismail Khan Refat <refatkhan897@gmail.com>
2026-01-19 02:45:42 +0600

    added post api

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 import express from "express";
+import { postRouter } from "./modules/post/post.router";
 const app = express()
 
+
+app.use(express.json());
+app.use("/posts", postRouter)
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
+import app from "./app";
 import { prisma } from "./lib/prisma"
-
+const PORT = process.env.PORT || 5000;
 async function main() {
     try {
         await prisma.$connect();
         console.log("Connected to database successfully");
 
 
+        app.listen(PORT, () => {
+            console.log(`server is running on PORT: ${PORT}`)
+        });
 
     } catch (error) {
-        console.log("An error occurred: ", error)
+        console.log("An error occurred: ", error);
+        await prisma.$disconnect();
+        process.exit(1);
     }
 }
+
+main()
