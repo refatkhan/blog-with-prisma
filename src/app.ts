775e48c4d3426e8d53977366f1be6f74efd4387b
Ismail Khan Refat <refatkhan897@gmail.com>
2026-01-19 16:51:45 +0600

    install better auth

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
 const app = express()
-
-
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use("/posts", postRouter)
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
